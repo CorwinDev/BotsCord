357a79bdd8488791baea0519a1df820b4026db64
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2022-08-15 19:47:50 +0200

    Update

--- website/routers/user/index.js
+++ website/routers/user/index.js
@@ -3,21 +3,34 @@ var express = require('express'),
 var servers = require('../../../models/server');
 var votes = require('../../../models/votes');
 var bots = require('../../../models/bot');
-
-router.get('/', function (req,res) {
+var users = require('../../../models/user');
+const user = require('../../../models/user');
+router.get('/', function (req, res) {
     res.redirect('/user/me')
 })
 router.get('/me', async function (req, res) {
-    if (req.user) {
-        var bot = await bots.find({ owners: req.user.id });
-        res.render('user/me', {
-            user: req.user,
-            bots: bot
-        });
-        return
-    } else {
+    if(!req.user){
         req.session.backURL = req.originalUrl;
-        res.redirect('/auth');
+        return res.redirect('/auth');
     }
+    var bot = await bots.find({ owners: req.user.id });
+    var user = await users.find({ id: req.user.id });
+    res.render('user/me', {
+        user: req.user,
+        bots: bot,
+        users: user,
+        dcuser: global.bsl.users.fetch(req.user.id)
+
+    });
 })
+router.get('/:id', async function (req, res) {
+    var bot = await bots.findOne({ id: req.params.id });
+    res.render('user/index', {
+        user: req.user,
+        bot: bot,
+        users: user,
+        dcuser: global.bsl.users.fetch(req.user.id)
+    });
+})
+
 module.exports = router;
