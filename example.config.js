b9a22a6cacf2732c7c608e91f9e322a19126a128
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2022-08-15 12:36:39 +0100

    Create example.config.js

---
+++ example.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+    maintenance: false,
+    mongodb: "",
+    bot: {
+        id: "",
+        token: "",
+        channels: {
+            login: "",
+            vote: "",
+            error: "",
+            info: "",
+        },
+        bsl: {
+            id: "",
+            token: "",
+            channels: {
+                vote: "",
+                error: "",
+            },
+            invite: ""
+        }
+    },
+    discord: {
+        id: "",
+        invite: "",
+        roles: {
+            admin: "",
+            mod: "",
+            user: "",
+            bot: ""
+        }
+    },
+    server: {
+        port: 3000,
+        host: "http://localhost:3000",
+        tags: [
+            "development",
+            "fun",
+            "media",
+            "company",
+            "game",
+            "emoji",
+            "turkish",
+            "support",
+            "sound",
+            "chatting",
+            "nsfw",
+            "challange"
+        ]
+    },
+    users: {
+        owner: [""],
+        verificator: [""],
+    }
+}
