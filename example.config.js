fde224ff72d8824cb5b365af83e98c5fd566720f
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2022-09-07 17:24:33 +0100

    Setup Before Open-Source??

---
+++ example.config.js
@@ -0,0 +1,58 @@
+module.exports = {
+    maintenance: false,
+    mongodb: "",
+    bot: {
+        id: "",
+        token: "",
+        channels: {
+            login: "868065455628234780",
+            vote: "937643610541793300",
+            error: "1003947531543269507",
+            info: "1005178285124239510",
+            new: "1005178285124239510",
+            admin: "1008736504325095444",
+            welcome: "873543353650806796"
+        },
+        bsl: {
+            id: "870001234583621713",
+            token: "",
+            channels: {
+                vote: "937643610541793300",
+                error: "1003947531543269507",
+            },
+            invite: "https://discord.com/api/oauth2/authorize?client_id=870001234583621713&permissions=314433&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback&response_type=code&scope=applications.commands%20bot"
+        }
+    },
+    discord: {
+        id: "868065454940385291",
+        invite: "",
+        roles: {
+            admin: "868065454940385291",
+            mod: "868065454940385291",
+            user: "868065454940385293",
+            bot: "1006120950045098034"
+        }
+    },
+    server: {
+        port: 3000,
+        host: "http://localhost:3000",
+        tags: [
+            "development",
+            "fun",
+            "media",
+            "company",
+            "game",
+            "emoji",
+            "turkish",
+            "support",
+            "sound",
+            "chatting",
+            "nsfw",
+            "challange"
+        ]
+    },
+    users: {
+        owner: ["755297485328482356"],
+        verificator: ["755297485328482356", "801547247882993665"],
+    }
+}
